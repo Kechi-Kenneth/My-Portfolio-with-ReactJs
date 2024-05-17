5f1a1f2dcf9e214d094b1e907693054e284ac6fc
Kechi-kenneth <kaypluzkechi@gmail.com>
2024-05-17 15:10:46 +0100

    friday

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import './App.css'
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Skills from "./Components/Skills/Skills";
 
 const App = () => {
-
   return (
     <>
-    < Navbar />
-    <div className="container">
-      <Hero />
+      <Navbar />
+      <div className="container">
+        <Hero />
+        <Skills />
       </div>
     </>
-  )
-}
+  );
+};
 
-
-
-export default App
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,36 @@
+ export const SKILLS = [
+  {
+    title: "Frontend",
+    icon: "./src/assets/icons8-frontend-48.png",
+    skills: [
+      {skills: "HTML5", percentage: "85%"},
+      {skills: "CSS3", percentage: "85%"},
+      {skills: "Javascript", percentage: "70%"},
+      {skills: "TypeScript", percentage: "50%"},
+      {skills: "React.Js", percentage: "65%"},
+    ],
+  },
+
+  {
+    title: "Tools",
+    icon: "./src/assets/icons8-tools-64.png",
+    skills: [
+      {skills: "Git & Github", percentage: "85%"},
+      {skills: "Visual Studio Code ", percentage: "70%"},
+      {skills: "Webpack", percentage: "70%"},
+      {skills: "Responsive Design", percentage: "70%"},
+      {skills: "API's", percentage: "65%"},
+    ],
+  },
+
+  {
+    title: "Soft-skills",
+    icon: "./src/assets/icons8-soft-skills-64.png",
+    skills: [
+      {skills: "Problem-Solving", percentage: "85%"},
+      {skills: "Algorithmn ", percentage: "70%"},
+      {skills: "Collaboration", percentage: "80%"},
+      {skills: "Attention to detauls", percentage: "85%"},
+    ],
+  },
+ ];
