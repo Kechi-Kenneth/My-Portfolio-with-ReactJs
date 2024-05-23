3642054a9597ed9d5d05c53fc7981ab8d7086dc3
Kechi-kenneth <kaypluzkechi@gmail.com>
2024-05-23 18:50:27 +0100

    extra features added

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      * 2024 Time to make a Mark,Happy Coding!
+    </div>
+  )
+}
+
+export default Footer;
