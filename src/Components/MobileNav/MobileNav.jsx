cf0c4faa5607de30f06c21b0b4f48f429e6c352a
Kechi-kenneth <kaypluzkechi@gmail.com>
2024-05-24 17:29:49 +0100

    functionality

--- src/Components/MobileNav/MobileNav.jsx
+++ src/Components/MobileNav/MobileNav.jsx
@@ -1,51 +1,48 @@
-// import React from "react";
-// import './MobileNav.css'
+import React from "react";
+import "./MobileNav.css";
 
-// const MobileNav = ({isOpen, toggleMenu }) => {
-//   return (
-//   <>
-//   <div
-//   className={`mobile-menu ${isOpen ? "active" : ""}`}
-//   onClick={toggleMenu}
-//   >
+const MobileNav = ({ isOpen, toggleMenu }) => {
+  return (
+    <>
+      <div
+        className={`mobile-menu ${isOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <div className="mobile-menu-container">
+          <div className="close-tab">
+             <button className="close-tab-button">
+              <img className="close-tab-icon" src="./public/icons8-close-50.png"/>
+             </button>
+          </div>
 
-// <div className="mobile-menu-container">
-//   <img className="nav-logo" src="" alt=""/>
+          <img className="nav-logo" src="" alt="" />
 
-// <ul>
+          <ul>
+            <li>
+              <a className="menu-item">Home</a>
+            </li>
 
-//   <li>
-//     <a className="menu-item">Home</a>
-//   </li>
+            <li>
+              <a className="menu-item">Skills</a>
+            </li>
+            <li>
+              <a className="menu-item">Education</a>
+            </li>
+            <li>
+              <a className="menu-item">Work Experience</a>
+            </li>
 
-//   <li>
-//   <a className="menu-item">Skills</a>
-// </li>
-// <li>
-//   <a className="menu-item">Education</a>
-// </li>
-// <li>
-//   <a className="menu-item">Work Experience</a>
-// </li>
+            <li>
+              <a className="menu-item">Contact Me</a>
+            </li>
+          </ul>
+          <button className="contact-me contact-again" onClick={() => {}}>
+            Hire Me
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-// <li>
-  
-//   <a className="menu-item">Contact Me</a>
-
-// </li>
-
-// </ul>
-// <button className="contact-me" onClick={() =>{}}>
-//     Click here
-//     </button>
-// </div>
-
-//   </div>
-  
-//   </>
-//   );
-    
-// };
-
-
-// export default MobileNav;
+export default MobileNav;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,64 +1,60 @@
 import React from "react";
-// import { useState } from "react";
-import './Navbar.css'
-
+import { useState } from "react";
+import "./Navbar.css";
+import MobileNav from "../MobileNav/MobileNav";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
 
-  // const {openMenu, setOpenMenu} = useState(false);
-  //  const toggleMenu = () => {
-    // setOpenMenu(!openMenu);
-  //  };
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
-return (
- <>
-{/* <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} /> */}
+  return (
+    <div>
+      <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+      <nav className="nav-wrapper">
+        <div className="nav-content">
+          <img className="nav-logo" src="" alt=""></img>
+          <ul>
+            <li>
+              <a className="menu-item">Home</a>
+            </li>
+            <li>
+              <a className="menu-item">Skills</a>
+            </li>
+            <li>
+              <a className="menu-item">Education</a>
+            </li>
+            <li>
+              <a className="menu-item">Work Experience</a>
+            </li>
 
- <nav className="nav-wrapper">
-   <div className="nav-content">
- <img className="nav-logo" src="" alt=""></img>
- <ul>
-<li>
-  <a className="menu-item">Home</a>
-</li>
-<li>
-  <a className="menu-item">Skills</a>
-</li>
-<li>
-  <a className="menu-item">Education</a>
-</li>
-<li>
-  <a className="menu-item">Work Experience</a>
-</li>
+            <li>
+              <a className="menu-item">Contact Me</a>
+            </li>
+            <button className="contact-me" onClick={() => {}}>
+              Hire Me
+            </button>
+          </ul>
 
-<li>
-  
-  <a className="menu-item">Contact Me</a>
-
-</li>
-<button className="contact-me" onClick={() =>{}}>
-    Hire Me 
-    </button>
-
- </ul>
-
-
- <button class="menu-btn" onClick={""}>
-  <span 
-  class={"material-symbols-outlined"}
-  style={{fontSize: "1.8rem"}}
-  >
-    <img className="menu-icon" src="src/assets/menu-symbol-of-three-parallel-lines-svgrepo-com.png" alt="" />
-    {/* {openMenu ? "close" : "menu"} */}
-  </span>
- </button>
-
-   </div>
-
- </nav>
- </>
-);
+          <button class="menu-btn" onClick={toggleMenu}>
+            <span
+              class={"material-symbols-outlined"}
+              style={{ fontSize: "1.8rem" }}
+            >
+              <img
+                className="menu-icon"
+                src="./public/menu-symbol-of-three-parallel-lines-svgrepo-com.png"
+                alt=""
+              />
+              {openMenu}
+            </span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
-
-export default Navbar
+export default Navbar;
